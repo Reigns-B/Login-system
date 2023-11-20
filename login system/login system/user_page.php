8c7e681819f62c4552887fdf6c40b34ec7c245ad
Reigns-B <148139147+Reigns-B@users.noreply.github.com>
2023-11-20 12:36:11 +0300

    Update user_page.php

--- login system/login system/user_page.php
+++ login system/login system/user_page.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['user_name'])){
       <p>this is an user page</p>
       <a href="Login.php" class="btn">login</a>
       <a href="register_form.php" class="btn">register</a>
-      <a href="logot.php" class="btn">logout</a>
+      <a href="logout.php" class="btn">logout</a>
    </div>
 
 </div>
